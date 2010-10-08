8aaff9452ecb392d51f3114da9a0218c30b7f201
Donovan Jimenez <djimenez@ptc.com>
2010-10-08 17:19:44 +0000

    See issue #49 - set curl options all at once using curl_setopt_array

--- Apache/Solr/HttpTransport/Curl.php
+++ Apache/Solr/HttpTransport/Curl.php
@@ -53,14 +53,14 @@ class Apache_Solr_HttpTransport_Curl extends Apache_Solr_HttpTransport_Abstract
 	 * SVN ID meta data for this class
 	 */
 	const SVN_ID = '$Id:$';
-	
+
 	/**
 	 * Curl Session Handle
-	 * 
+	 *
 	 * @var resource
 	 */
 	private $_curl;
-	
+
 	/**
 	 * Initializes a curl session
 	 */
@@ -68,18 +68,20 @@ class Apache_Solr_HttpTransport_Curl extends Apache_Solr_HttpTransport_Abstract
 	{
 		// initialize a CURL session
 		$this->_curl = curl_init();
-		
+
 		// set common options that will not be changed during the session
-		//   first, return the response body from curl_exec
-		curl_setopt($this->_curl, CURLOPT_RETURNTRANSFER, true);
-		
-		//  second, get the binary output
-		curl_setopt($this->_curl, CURLOPT_BINARYTRANSFER, true);
-		
-		// third, we do not need the headers in the output, we get everything we need from meta data
-		curl_setopt($this->_curl, CURLOPT_HEADER, false);
+		curl_setopt_array($this->_curl, array(
+			// return the response body from curl_exec
+			CURLOPT_RETURNTRANSFER => true,
+
+			// get the output as binary data
+			CURLOPT_BINARYTRANSFER => true,
+
+			// we do not need the headers in the output, we get everything we need from curl_getinfo
+			CURLOPT_HEADER => false
+		));
 	}
-		
+
 	/**
 	 * Closes a curl session
 	 */
@@ -88,100 +90,109 @@ class Apache_Solr_HttpTransport_Curl extends Apache_Solr_HttpTransport_Abstract
 		// close our curl session
 		curl_close($this->_curl);
 	}
-	
+
 	public function performGetRequest($url, $timeout = false)
 	{
-		// make sure we're returning the body
-		curl_setopt($this->_curl, CURLOPT_NOBODY, false);
-		
-		// make sure we're GET
-		curl_setopt($this->_curl, CURLOPT_HTTPGET, true);
-		
-		// set the URL
-		curl_setopt($this->_curl, CURLOPT_URL, $url);
-		
-		// set the timeout if specified
-		if ($timeout !== FALSE && $timeout > 0.0)
+		// check the timeout value
+		if ($timeout === false || $timeout <= 0.0)
 		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $timeout);
+			// use the default timeout
+			$timeout = $this->getDefaultTimeout();
 		}
-		else
-		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $this->getDefaultTimeout());
-		}
-		
+
+		// set curl GET options
+		curl_setopt_array($this->_curl, array(
+			// make sure we're returning the body
+			CURLOPT_NOBODY => false,
+
+			// make sure we're GET
+			CURLOPT_HTTPGET => true,
+
+			// set the URL
+			CURLOPT_URL => $url,
+
+			// set the timeout
+			CURLOPT_TIMEOUT => $timeout
+		));
+
 		// make the request
 		$responseBody = curl_exec($this->_curl);
-		
+
 		// get info from the transfer
 		list($statusCode, $contentType, $encoding) = $this->_getHeaderInformation();
-				
+
 		return new Apache_Solr_HttpTransport_Response($statusCode, null, $responseBody, $contentType, $encoding);
-	}	
-	
+	}
+
 	public function performHeadRequest($url, $timeout = false)
 	{
-		// this both sets the method to HEAD and says not to return a body
-		curl_setopt($this->_curl, CURLOPT_NOBODY, true);
-		
-		// set the URL
-		curl_setopt($this->_curl, CURLOPT_URL, $url);
-		
-		// set the timeout if specified
-		if ($timeout !== FALSE && $timeout > 0.0)
+		// check the timeout value
+		if ($timeout === false || $timeout <= 0.0)
 		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $timeout);
+			// use the default timeout
+			$timeout = $this->getDefaultTimeout();
 		}
-		else
-		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $this->getDefaultTimeout());
-		}
-		
+
+		// set curl HEAD options
+		curl_setopt_array($this->_curl, array(
+			// this both sets the method to HEAD and says not to return a body
+			CURLOPT_NOBODY => true,
+
+			// set the URL
+			CURLOPT_URL => $url,
+
+			// set the timeout
+			CURLOPT_TIMEOUT => $timeout
+		));
+
 		// make the request
 		$responseBody = curl_exec($this->_curl);
-		
+
 		// get info from the transfer
 		list($statusCode, $contentType, $encoding) = $this->_getHeaderInformation();
-				
+
 		return new Apache_Solr_HttpTransport_Response($statusCode, null, $responseBody, $contentType, $encoding);
 	}
-	
+
 	public function performPostRequest($url, $postData, $contentType, $timeout = false)
 	{
-		// make sure we're returning the body
-		curl_setopt($this->_curl, CURLOPT_NOBODY, false);
-		
-		// make sure we're POST
-		curl_setopt($this->_curl, CURLOPT_POST, true);
-		
-		// set the URL
-		curl_setopt($this->_curl, CURLOPT_URL, $url);
-		
-		// set the post data
-		curl_setopt($this->_curl, CURLOPT_POSTFIELDS, $postData);
-		
-		// set the content type
-		curl_setopt($this->_curl, CURLOPT_HTTPHEADER, array("Content-Type: {$contentType}"));
-		
-		// set the timeout if specified
-		if ($timeout !== FALSE && $timeout > 0.0)
+		// check the timeout value
+		if ($timeout === false || $timeout <= 0.0)
 		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $timeout);
+			// use the default timeout
+			$timeout = $this->getDefaultTimeout();
 		}
-		else
-		{
-			curl_setopt($this->_curl, CURLOPT_TIMEOUT, $this->getDefaultTimeout());
-		}
-		
+
+		// set curl POST options
+		curl_setopt_array($this->_curl, array(
+			// make sure we're returning the body
+			CURLOPT_NOBODY => false,
+
+			// make sure we're POST
+			CURLOPT_PORT => true,
+
+			// set the URL
+			CURLOPT_URL => $url,
+
+			// set the post data
+			CURLOPT_POSTFIELDS => $postData,
+
+			// set the content type
+			CURLOPT_HTTPHEADER => array("Content-Type: {$contentType}"),
+
+			// set the timeout
+			CURLOPT_TIMEOUT => $timeout
+		));
+
 		// make the request
 		$responseBody = curl_exec($this->_curl);
-		
+
 		// get info from the transfer
 		list($statusCode, $contentType, $encoding) = $this->_getHeaderInformation();
-				
+
 		return new Apache_Solr_HttpTransport_Response($statusCode, null, $responseBody, $contentType, $encoding);
 	}
-	
+
 	/**
 	 * Get information from the last transfer
 	 *
@@ -192,13 +203,13 @@ class Apache_Solr_HttpTransport_Curl extends Apache_Solr_HttpTransport_Abstract
 		// defaults
 		$type = 'text/plain';
 		$encoding = 'UTF-8';
-		
+
 		// first get status code
 		$statusCode = curl_getinfo($this->_curl, CURLINFO_HTTP_CODE);
-		
+
 		// get the content type
 		$contentTypeHeader = curl_getinfo($this->_curl, CURLINFO_CONTENT_TYPE);
-		
+
 		if ($contentTypeHeader)
 		{
 			// now break apart the header to see if there's character encoding
@@ -208,19 +219,19 @@ class Apache_Solr_HttpTransport_Curl extends Apache_Solr_HttpTransport_Abstract
 			{
 				$type = trim($contentTypeParts[0]);
 			}
-			
+
 			if (isset($contentTypeParts[1]))
 			{
 				// we have a second part, split it further
 				$contentTypeParts = explode('=', $contentTypeParts[1]);
-				
+
 				if (isset($contentTypeParts[1]))
 				{
 					$encoding = trim($contentTypeParts[1]);
 				}
 			}
 		}
-		
+
 		return array($statusCode, $type, $encoding);
 	}
 }
